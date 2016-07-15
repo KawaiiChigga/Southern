91a1597dce8a509c0ee3a84f3ea9bf4cb64043fb
gaunled <gaunled@gmail.com>
2016-07-15 16:37:11 +0700

    tambah model order

---
+++ src/View/staff/VChef.java
@@ -0,0 +1,43 @@
+package View.staff;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class VChef extends JPanel{
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private int screenWidth = screenSize.width;
+	private int screenHeight = screenSize.height;
+	
+	public VChef(JFrame frame){
+		this.frame = frame;
+		initVChef();
+	}
+
+	private void initVChef() {
+		// TODO Auto-generated method stub
+		setPreferredSize(new Dimension(screenWidth, screenHeight));
+		setBackground(Color.WHITE);
+		setLayout(null);
+		
+		bill = new JButton();
+		bill.setText("Bill");
+		bill.setBounds(200, 200, 200, 200);
+		
+		checkTrans = new JButton();
+		checkTrans.setText("Check Transaction");
+		checkTrans.setBounds(600, 200, 200, 200);
+		
+		add(bill);
+		add(checkTrans);
+	}
+	private JButton bill;
+	private JButton checkTrans;
+	private JFrame frame;
+	private Image bg;
+}
